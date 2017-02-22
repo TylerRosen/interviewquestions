5544ef44949077191ef61f82c803b0abb955f926
TylerRosen <tylermrosen@gmail.com>
2017-02-21 21:30:47 -0500

    Solved problem for printEvenBetween

--- interview.js
+++ interview.js
@@ -15,14 +15,14 @@ greatThanThree(4, 5);
 // return false.
 
 function aOrB(letter) {
-    if (letter == a) {
+    if (letter == "a") {
         return true;
     } else {
         return false;
     };
 };
 
-aOrB(a)
+aOrB("a");
 
 // Write a function named greatThanTwo that returns true if the number is 2 and false otherwise
 
@@ -43,13 +43,13 @@ function printEvenBetween(numOne, numTwo) {
     var numbs = [];
 
     for (var i = numOne + 1; i < numTwo; i++) {
-        numbs.push(i);
-        if (numbs[i] % 2 == 0) {
-            return numbs;
+        if (i % 2 == 0) {
+            numbs.push(i);
+          
         }
-    }
+    } return numbs;
 
-}
+ }
 
 // Write a function named printBetween that takes in two arguments. Assume they are integers. 
 // This function will return an array of the numbers between those two arguments.
@@ -66,3 +66,25 @@ function printBetween(a, b) {
 // Write a function named fib that takes an argument n. Assume that n is a number. 
 // And the function returns an array of n numbers of the fibonacci sequence.
 
+var arr = [1, 2, 3]
+
+//last
+arr[length-1] = arr[3-1] = arr[2]
+
+//second to last
+arr[length-2] = arr[3-2] = arr[1]
+
+function fib(n){ // n=3
+    if (n==0) return [];
+    if (n==1) return [0];
+    if (n==2) return [0,1];
+
+    var fibList = [0, 1]; // [0,1, 1]
+    for (var i=2; i<n; i++){
+        var last = fibList[fibList.length-1];
+        var secondToLast = fibList[fibList.length-2];
+        fibList.push(last + secondToLast);
+    }
+
+    return fibList;
+}
